04f9547f379a964159fcc7e1ec3af4006b877340
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-05-04 13:43:33 +0300

    add

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,18 +1,22 @@
 const { HttpError } = require('../helpers/');
 
+const errMessage = fieldName => `missing required ${fieldName} field`;
+
 const validated = (shema) => {
     const func = (req, res, next) => {
         const { name, email, phone } = req.body;
         if (!name && !email && !phone) next(HttpError(400, "missing fields"));
-        if (!name) next(HttpError(400, "missing required name field"));
-        if (!email) next(HttpError(400, "missing required email field"));
-        if (!phone) next(HttpError(400, "missing required phone field"));
+        if (!name) next(HttpError(400, errMessage('name')));
+        if (!email) next(HttpError(400, errMessage('email')));
+        if (!phone) next(HttpError(400, errMessage('phone')));
         const { error } = shema.validate(req.body);
         if (error) next(HttpError(400, error.message));
         next();
     };
     return func;
 };
+
+
 
 module.exports = validated;
 
